3b141156a4bfcdc3df3552699709668ed086d7ee
Framklin <48841072+josedavila2745@users.noreply.github.com>
2020-07-07 18:34:48 -0500

    maquetado

--- app/src/main/java/com/example/calculadora1/MainActivity.kt
+++ app/src/main/java/com/example/calculadora1/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.calculadora1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var resul : Float = 0.0f
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var res: TextView = findViewById(R.id.resultado)
     }
 }
